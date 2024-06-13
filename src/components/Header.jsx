01476606a10ed39152442cff823ef9c5f4d13a21
unknown <gsmahato8@gmail.com>
2024-06-13 11:55:54 +0545

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import logo from "../assets/logo.png.webp"
-import { Menu, X, Facebook, Instagram } from "lucide-react"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import logo from "../assets/logo.png.webp";
+import { Menu, X, Facebook, Instagram } from "lucide-react";
 
 const links = [
   { name: "Home", url: "/" },
@@ -11,16 +11,32 @@ const links = [
   { name: "Generic", url: "/generic" },
   { name: "Elements", url: "/elements" },
 ];
-function Header() {
 
+function Header() {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const [navbarBg, setNavbarBg] = useState("bg-transparent");
 
   const toggleNavbar = () => {
-    setMobileDrawerOpen(!mobileDrawerOpen)
+    setMobileDrawerOpen(!mobileDrawerOpen);
   };
 
+  const changeNavbarBg = () => {
+    if (window.scrollY >= 80) {
+      setNavbarBg("bg-black/90");
+    } else {
+      setNavbarBg("bg-transparent");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavbarBg);
+    return () => {
+      window.removeEventListener("scroll", changeNavbarBg);
+    };
+  }, []);
+
   return (
-    <header className="sticky top-0 z-50 py-3 backdrop-blur-lg bg-black/90 border-b border-neutral-700/80">
+    <header className={`sticky top-0 z-50 py-3 ${navbarBg}`}>
       <div className="px-4 lg:px-24 mx-auto relative text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center justify-between w-full">
@@ -31,19 +47,11 @@ function Header() {
             </div>
             <ul className="hidden lg:flex space-x-12">
               {links.map((link, index) => (
-                <a key={index} href={link.url} className=" text-white text-sm font-semibold uppercase mx-6">
+                <a key={index} href={link.url} className="text-white text-sm font-semibold uppercase mx-6">
                   {link.name}
                 </a>
               ))}
             </ul>
-            {/* <div className="hidden lg:flex space-x-1">
-              <a href="https://www.facebook.com" className="mx-4" target="_blank" rel="noopener noreferrer">
-                <Facebook />
-              </a>
-              <a href="https://www.instagram.com" className="mx-4" target="_blank" rel="noopener noreferrer">
-                <Instagram />
-              </a>
-            </div> */}
             <div className="lg:hidden md:flex flex-col justify-end flex">
               <button onClick={toggleNavbar}>
                 {mobileDrawerOpen ? <X className="text-white" /> : <Menu className="text-white" />}
@@ -52,7 +60,7 @@ function Header() {
           </div>
         </div>
         {mobileDrawerOpen && (
-          <div className="fixed right-0 z-20 bg-black/90 w-full p-12 flex flex-col mt-3 justify-center item-center lg:hideen">
+          <div className="fixed right-0 z-20 bg-black/90 w-full p-12 flex flex-col mt-3 justify-center items-center lg:hidden">
             <ul className="space-y-4 text-center">
               {links.map((link, index) => (
                 <a key={index} href={link.url} className="block text-white text-sm font-semibold uppercase py-2">
@@ -64,7 +72,7 @@ function Header() {
         )}
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
